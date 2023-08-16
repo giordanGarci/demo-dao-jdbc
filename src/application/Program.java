6fb342e80d97275b448dc190907d627bbfce328f
Giordan Garcia <giordangarcia12@gmail.com>
2023-08-16 20:27:31 -0300

    Creating DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,10 @@
 package application;
 
-public class Program {
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 
+public class Program {
+	public static void main(String[] args) {
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+	}
 }
