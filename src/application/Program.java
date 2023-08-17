0e6bdfbf08eae7221189477c4c17e620bb57c9c4
Giordan Garcia <giordangarcia12@gmail.com>
2023-08-17 15:35:21 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -32,5 +32,11 @@ public class Program {
 		Seller obj = new Seller(null, "carlos", "carlos@gmail.com", new Date(), 4000.0, dep);
 		sellerDao.insert(obj);
 		System.out.println("Inserted! new Id: " + obj.getId()); 
+		
+		System.out.println("\n=== TEST 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
+		System.out.println("Update complete!");
 	}
 }
